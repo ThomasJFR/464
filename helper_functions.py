0aba5c467002bdc1b50f2a5fd97813880f539a93
Thomas <thomas.joakim@gmail.com>
2023-03-11 15:45:33 -0800

    Added another function

--- helper_functions.py
+++ helper_functions.py
@@ -22,4 +22,14 @@ def knock_over_blue_bowl(manipulator):
     move_cp(manipulator, 0, 0.02, -0.12).wait()
     move_cp(manipulator, 0.20, 0, 0).wait()
 
+def crush_blue_bowl(m1, m2):
+    ## UNFINISHED 
+    # Move to base
+    m1wait = move_vertical_safely(m1)
+    move_vertical_safely(m2).wait()
+    m1wait.wait()
+
+    move_cp(m1, 0, 0.02, -0.12).wait()
+    move_cp(m2, 0, -0.05, -0.12).wait()
     
+
