f21a54872ad34ea686ad4937a8feae48e3f11a7e
Shelby Quiring <shelbyaquiring@gmail.com>
2023-03-19 13:01:46 -0700

    Adding trajectory generation files

---
+++ trajGen.py
@@ -0,0 +1,14 @@
+#%%
+"""
+This is a function that generates a trajectory for the DVRK robot.
+
+INPUTS:
+
+
+OUTPUTS:
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from dvrk import *
