20d2911316eeec88bdcd8b35ddc5a3e10d966a8a
Thomas <thomas.joakim@gmail.com>
2023-03-29 16:26:49 -0700

    Added calibration fun

---
+++ MAIN/calibration.py
@@ -0,0 +1,10 @@
+def calibrate_z(psm):
+    """
+    WILL HANG THE PROGRAM
+    """
+    # Hang the program so the user can place the arm
+    input("Press enter to continue")
+
+    # Extract arm position
+    z = psm.measured_cp().p[2]
+    return z
