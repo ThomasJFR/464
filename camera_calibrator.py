b708564122690b328b82f7ed3281a8a06acebb94
fizzer <dng.1259@gmail.com>
2023-03-29 17:35:03 -0700

    updated with undistortion

--- camera_calibrator.py
+++ camera_calibrator.py
@@ -1,34 +1,45 @@
 import numpy as np
 import cv2 as cv
 import glob
+import os
 
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((5*8,3), np.float32)
+objp[:,:2] = np.mgrid[0:5,0:8].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-images = glob.glob('checkerboard_photo.jpg')
 
+# resize images
+
+images = glob.glob(os.path.join("./calibration photos", "*.jpg"))
 
 for fname in images:
     img = cv.imread(fname)
+
+    # resize images
+    print('Original Dimensions : ',img.shape)
+    scale_percent = 20 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+    print('Resized Dimensions : ',img.shape)
+
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    cv.resize
+
     cv.imshow('gray',gray)
     cv.waitKey(5000)
 
     # Find the (interior) chess board corners
     ret, corners = cv.findChessboardCorners(gray, (5,8), None)
-    print(ret)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
-        print('in if statement')
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
         imgpoints.append(corners2)
@@ -38,3 +49,17 @@ for fname in images:
         cv.imshow('img', img)
 
 cv.destroyAllWindows()
+
+# Calibration
+# get camera matrix, distortion coeffs, rotation/translation vectors
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+# Undistortion
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult.png', dst)
