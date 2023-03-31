e96407931ba24a12db8856585aaa4c9b1fe18ccb
Daniel <dng.1259@gmail.com>
2023-03-31 16:54:12 -0700

    updated user inputs

--- camera_calibrator.py
+++ camera_calibrator.py
@@ -3,6 +3,19 @@ import cv2 as cv
 import glob
 import os
 
+# -----------------USER INPUT-----------------
+# checkerboard size (count interior corners only, eg. a chessboard is 7x7 not 9x9)
+rows = 6
+columns = 7
+
+# image rescale percentage
+scale_percent = 50
+
+# calibration photos array
+images = glob.glob(os.path.join("./calibration photos", "*.png"))
+
+#---------------------------------------------
+
 def resize(img, scale_percent):
     print('Original Dimensions : ',img.shape)
     width = int(img.shape[1] * scale_percent / 100)
@@ -16,15 +29,12 @@ def resize(img, scale_percent):
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((5*8,3), np.float32)
-objp[:,:2] = np.mgrid[0:5,0:8].T.reshape(-1,2)
+objp = np.zeros((rows*columns,3), np.float32)
+objp[:,:2] = np.mgrid[0:rows,0:columns].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-
-images = glob.glob(os.path.join("./calibration photos", "*.jpg"))
-scale_percent = 20
 
 for fname in images:
     img = cv.imread(fname)
@@ -35,10 +45,11 @@ for fname in images:
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     cv.imshow('gray',gray)
-    cv.waitKey(1000)
+    cv.waitKey(500)
 
     # Find the (interior) chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (5,8), None)
+    ret, corners = cv.findChessboardCorners(gray, (rows,columns), None)
+    print(ret)
 
     # If found, add object points, image points (after refining them)
     if ret == True:
@@ -47,7 +58,7 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.drawChessboardCorners(img, (rows,columns), corners2, ret)
         cv.imshow('img', img)
 
 cv.destroyAllWindows()
@@ -57,7 +68,7 @@ cv.destroyAllWindows()
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 # Undistortion
-img = cv.imread('./calibration photos/20230327_230528.jpg')
+img = cv.imread('./calibration photos/cameraCal7.png')
 img = resize(img,scale_percent)
 h,  w = img.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
